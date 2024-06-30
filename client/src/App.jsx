2ded66a127fa1504a1ca1df4f0160f7ea13c443e
jitender <cjatinder39@gmail.com>
2024-06-30 15:38:14 +0530

    completed header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import {BrowserRouter, Route, Routes } from "react-router-dom"
+import Header from './components/Header'
 import Home from './pages/Home'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
@@ -12,6 +13,7 @@ function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
