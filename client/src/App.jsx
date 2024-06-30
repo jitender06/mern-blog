68c1e335790730ea9e71cd37be3782d03dbe8b7b
jitender <cjatinder39@gmail.com>
2024-06-30 14:43:08 +0530

    created necessary routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import {BrowserRouter, Route, Routes } from "react-router-dom"
-import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +13,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Header/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
       </Routes>
     </BrowserRouter>
   )
